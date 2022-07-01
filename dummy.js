622d1b9ae61e742aa981096a4f8c7d31076836c8
Muhammad Mudassir <104167723+MuhammMud@users.noreply.github.com>
2022-06-30 22:16:34 -0400

    Add files via upload
    Constants folder.

---
+++ dummy.js
@@ -0,0 +1,194 @@
+import assets from "./assets";
+
+const NFTData = [
+  {
+    id: "NFT-01",
+    name: "Abstracto #980",
+    creator: "Itri Panta",
+    price: 3.55,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.",
+    image: assets.nft01,
+    bids: [
+      {
+        id: "BID-11",
+        name: "Lucy Kang",
+        price: 4.25,
+        image: assets.person02,
+        date: "March 12, 2022 at 12:10 PM",
+      },
+      {
+        id: "BID-12",
+        name: "Angela Hartley",
+        price: 4.5,
+        image: assets.person03,
+        date: "March 13, 2022 at 1:50 PM",
+      },
+      {
+        id: "BID-13",
+        name: "Rose Dameier",
+        price: 4.75,
+        image: assets.person04,
+        date: "March 13, 2022 at 6:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-02",
+    name: "Green Energy",
+    creator: "Siti Norhai",
+    price: 7.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+    image: assets.nft02,
+    bids: [
+      {
+        id: "BID-21",
+        name: "Lucy Kang",
+        price: 7.05,
+        image: assets.person02,
+        date: "December 12, 2021 at 12:10 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-03",
+    name: "NFT Coin Jumble",
+    creator: "Vladimir Lezin",
+    price: 95.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+    image: assets.nft03,
+    bids: [
+      {
+        id: "BID-31",
+        name: "Lucy Kang",
+        price: 95.25,
+        image: assets.person02,
+        date: "January 12, 2022 at 12:10 PM",
+      },
+      {
+        id: "BID-32",
+        name: "Angela Hartley",
+        price: 95.5,
+        image: assets.person03,
+        date: "Jan 17, 2022 at 1:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-04",
+    name: "Nifty NFT",
+    creator: "David Alahoi",
+    price: 54.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur.",
+    image: assets.nft04,
+    bids: [
+      {
+        id: "BID-41",
+        name: "Jose Alvarez",
+        price: 56.25,
+        image: assets.person08,
+        date: "December 12, 2021 at 12:10 PM",
+      },
+      {
+        id: "BID-42",
+        name: "Angela Hartley",
+        price: 54.25,
+        image: assets.person03,
+        date: "December 27, 2021 at 1:50 AM",
+      },
+      {
+        id: "BID-43",
+        name: "Rose Dameier",
+        price: 55.15,
+        image: assets.person04,
+        date: "December 31, 2021 at 3:50 PM",
+      },
+      {
+        id: "BID-44",
+        name: "Vincent Swift",
+        price: 54.15,
+        image: assets.person05,
+        date: "December 31, 2021 at 3:50 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-05",
+    name: "Colorful Circles",
+    creator: "Yusuf Mazar",
+    price: 10.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural.",
+    image: assets.nft05,
+    bids: [
+      {
+        id: "BID-51",
+        name: "Lucy Kang",
+        price: 10.25,
+        image: assets.person02,
+        date: "February 12, 2022 at 12:10 PM",
+      },
+    ],
+  },
+  {
+    id: "NFT-06",
+    name: "Black Box",
+    creator: "Reo Mansini",
+    price: 20.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis.",
+    image: assets.nft06,
+    bids: [
+      {
+        id: "BID-61",
+        name: "Luke Maan",
+        price: 20.25,
+        image: assets.person06,
+        date: "April 12, 2022 at 11:10 AM",
+      },
+      {
+        id: "BID-62",
+        name: "Angela Hartley",
+        price: 20.5,
+        image: assets.person01,
+        date: "April 17, 2022 at 1:50 PM",
+      },
+      {
+        id: "BID-63",
+        name: "Rose Dameier",
+        price: 20.75,
+        image: assets.person03,
+        date: "April 21, 2022 at 3:50 PM",
+      },
+      {
+        id: "BID-64",
+        name: "Siti Norhai",
+        price: 21.25,
+        image: assets.person04,
+        date: "April 22, 2022 at 9:50 PM",
+      },
+      {
+        id: "BID-65",
+        name: "Kaitlyn Lee",
+        price: 7.25,
+        image: assets.person07,
+        date: "April 25, 2022 at 9:50 AM",
+      },
+    ],
+  },
+  {
+    id: "NFT-07",
+    name: "Prism Chasm",
+    creator: "Ernesto de la Cruz",
+    price: 17.25,
+    description:
+      "The action painter abstract expressionists were directly influenced by automatism. Pollock channelled this into producing gestural. Lorem ipsum dolor sit amet consectetur adipiscing elit consequat accumsan sapien, lectus convallis malesuada odio curae habitasse dignissim nascetur. Nulla sed velit erat vitae leo sem inceptos diam fames arcu hendrerit, quis ultrices in eleifend posuere ipsum conubia porttitor felis. Ullamcorper platea penatibus ornare egestas nulla ligula hendrerit nisl suscipit sociosqu maximus, tincidunt aptent habitant purus pharetra ultrices dapibus laoreet nisi lacinia. Porta malesuada netus vel sapien conubia porttitor aliquam ut pretium ante litora molestie senectus magna egestas sociosqu, eget aliquet fames pharetra felis posuere varius fringilla quisque in arcu montes eu ullamcorper.",
+    image: assets.nft07,
+    bids: [],
+  },
+];
+
+export { NFTData };
--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import HomeHeader from "./HomeHeader";
-import FocusedStatusBar from "./FocusedStatusBar";
-import NFTCard from "./NFTCard";
-import { CircleButton, RectButton } from "./Button";
-import DetailsDesc from "./DetailsDesc";
-import DetailsBid from "./DetailsBid";
-import { SubInfo } from "./SubInfo";
-export {
-    HomeHeader, NFTCard, FocusedStatusBar,
-    CircleButton, RectButton, DetailsBid, 
-    DetailsDesc, SubInfo
-}
+import assets from "./assets";
+import { COLORS, SHADOWS, SIZES, FONTS } from "./theme";
+import { NFTData } from "./dummy";
+
+export { assets, COLORS, SHADOWS, SIZES, FONTS, NFTData };
